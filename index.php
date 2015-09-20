456470123dbc5fd736b5b38ff9c6df53ab8a31f3
Mallory Spector <malloryspector@gmail.com>
2015-09-20 17:28:08 -0400

    Ability to add random number and symbol based on user preferences

--- index.php
+++ index.php
@@ -7,27 +7,26 @@ ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 <html>
   <head>
     <title>xkcd Style Password Generator</title>
-    <meta charset="utf-8" />
+    <meta charset='utf-8' />
     <?php require 'logic.php'; ?>
   </head>
 
   <body>
     <h1>xkcd Style Password Generator</h1>
-    
+
     <?php echo $password; ?>
 
     <form method='POST' action='index.php'>
 
-      <input type='text' name='number_of_words'>
+      <input type='text' name='number_of_words' value="<?php if (isset($_POST['number_of_words'])) { echo $_POST['number_of_words']; } ?>">
       <label for="number_of_words">Number of words</label><br>
-      <input type='checkbox' name='add_number'>
+      <input type='checkbox' name='add_number' <?php if (isset($_POST['add_number'])) { echo "checked='checked'"; } ?> >
       <label for='add_number'>Add a number</label><br>
-      <input type='checkbox' name='add_symbol'>
+      <input type='checkbox' name='add_symbol' <?php if (isset($_POST['add_symbol'])) { echo "checked='checked'"; } ?> >
       <label for="add_symbol">Add a symbol</label><br>
       <input type='submit' value='Generate My Password!'>
 
     </form>
-
 
   </body>
 </html>
--- logic.php
+++ logic.php
@@ -2,16 +2,19 @@
 // Create an array of random words
 $words = array("notice", "yarn", "want", "second", "cat", "impolite", "pump", "playground", "blue", "box", "day", "produce", "table", "sheet", "apparatus", "protect", "late", "house", "lumpy", "wooden");
 
+// Create an array of random symbols
+$symbols = array("!", "@", "#", "$", "%", "?", "&", "*");
+
 // Default word total
 $word_total = 4;
 
 /*
-First check to see if an array key is available to use in $_POST array
+First check to see if a variable is set for "number_of_words" in $_POST
 Set the number of words for each password to the variable $word_total
 If a value is entered by the user, set word total to that number
 If no value is entered default to 4
 */
-if (array_key_exists("number_of_words", $_POST)) {
+if (isset($_POST["number_of_words"])) {
   if ($_POST["number_of_words"] != null) {
     $word_total = $_POST["number_of_words"];
   } else {
@@ -22,13 +25,34 @@ if (array_key_exists("number_of_words", $_POST)) {
 // Create an array of chosen words
 $password_words = array();
 
-// Generate password based on the value of $word_total
+/*
+Generate password based on the value of $word_total
+Get a random word and set that random word into our chosen words array
+*/
 for ($i = 0; $i < $word_total; $i++) {
-  // Get a random word
   $rand_word = $words[rand(0, count($words)-1)];
-  // Place our random word into our chosen word array
   $password_words[$i] = $rand_word;
 }
 
-// Join array elements with "-" string to get final password
+/*
+Add number at the end of a random word if checked
+*/
+if (isset($_POST["add_number"])) {
+  $add_number = rand(1, 9);
+  $num_place = rand(0, count($password_words)-1);
+  $password_words[$num_place] = $password_words[$num_place] . $add_number;
+}
+
+/*
+Add symbol at the end of a random word if checked
+*/
+if (isset($_POST["add_symbol"])) {
+  $add_symbol = $symbols[rand(0, count($symbols)-1)];
+  $sym_place = rand(0, count($password_words)-1);
+  $password_words[$sym_place] = $password_words[$sym_place] . $add_symbol;
+}
+
+/*
+Join array elements with "-" string to get final password
+*/
 $password = implode ("-", $password_words);
