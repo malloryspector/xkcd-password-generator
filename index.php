571239490ccc386918c3a3f35acadc32c6dd777f
Mallory Spector <malloryspector@gmail.com>
2015-09-20 23:31:35 -0400

    Specify quantity of numbers and symbols, add css file

--- index.php
+++ index.php
@@ -1,32 +1,50 @@
 <?php
 error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
-ini_set('display_errors', 1); # Display errors on page (instead of a log file)
+ini_set("display_errors", 1); # Display errors on page (instead of a log file)
 ?>
 
 <!DOCTYPE html>
 <html>
   <head>
     <title>xkcd Style Password Generator</title>
-    <meta charset='utf-8' />
-    <?php require 'logic.php'; ?>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+    <?php require "logic.php"; ?>
   </head>
 
   <body>
-    <h1>xkcd Style Password Generator</h1>
+    <div class="container">
+      <h1>xkcd Style Password Generator</h1>
 
-    <?php echo $password; ?>
+      <div class="password">
+        <?php echo $password; ?>
+      </div>
 
-    <form method='POST' action='index.php'>
+      <form method="POST" action="index.php">
 
-      <input type='text' name='number_of_words' value="<?php if (isset($_POST['number_of_words'])) { echo $_POST['number_of_words']; } ?>">
-      <label for="number_of_words">Number of words</label><br>
-      <input type='checkbox' name='add_number' <?php if (isset($_POST['add_number'])) { echo "checked='checked'"; } ?> >
-      <label for='add_number'>Add a number</label><br>
-      <input type='checkbox' name='add_symbol' <?php if (isset($_POST['add_symbol'])) { echo "checked='checked'"; } ?> >
-      <label for="add_symbol">Add a symbol</label><br>
-      <input type='submit' value='Generate My Password!'>
+        <input type="text" name="number_of_words" maxlength="1" value="<?php if (isset($_POST["number_of_words"])) { echo $_POST["number_of_words"]; } ?>">
+        <label for="number_of_words">Number of words (Max 9)</label><br>
+        <input type="checkbox" name="add_number" <?php if (isset($_POST["add_number"])) { echo "checked='checked'"; } ?> >
+        <label for="add_number">Add a number</label>
+        <select name="additional_numbers">
+          <option value="1" <?php if (isset($_POST["additional_numbers"]) && $_POST["additional_numbers"] == 1) { echo "selected='true'"; } ?>>1</option>
+          <option value="2" <?php if (isset($_POST["additional_numbers"]) && $_POST["additional_numbers"] == 2) { echo "selected='true'"; } ?>>2</option>
+          <option value="3" <?php if (isset($_POST["additional_numbers"]) && $_POST["additional_numbers"] == 3) { echo "selected='true'"; } ?>>3</option>
+          <option value="4" <?php if (isset($_POST["additional_numbers"]) && $_POST["additional_numbers"] == 4) { echo "selected='true'"; } ?>>4</option>
+          <option value="5" <?php if (isset($_POST["additional_numbers"]) && $_POST["additional_numbers"] == 5) { echo "selected='true'"; } ?>>5</option>
+        </select><br>
+        <input type="checkbox" name="add_symbol" <?php if (isset($_POST["add_symbol"])) { echo "checked='checked'"; } ?> >
+        <label for="add_symbol">Add a symbol</label>
+        <select name="additional_symbols">
+          <option value="1" <?php if (isset($_POST["additional_symbols"]) && $_POST["additional_symbols"] == 1) { echo "selected='true'"; } ?>>1</option>
+          <option value="2" <?php if (isset($_POST["additional_symbols"]) && $_POST["additional_symbols"] == 2) { echo "selected='true'"; } ?>>2</option>
+          <option value="3" <?php if (isset($_POST["additional_symbols"]) && $_POST["additional_symbols"] == 3) { echo "selected='true'"; } ?>>3</option>
+          <option value="4" <?php if (isset($_POST["additional_symbols"]) && $_POST["additional_symbols"] == 4) { echo "selected='true'"; } ?>>4</option>
+          <option value="5" <?php if (isset($_POST["additional_symbols"]) && $_POST["additional_symbols"] == 5) { echo "selected='true'"; } ?>>5</option>
+        </select><br>
+        <input type="submit" value="Generate My Password!">
 
-    </form>
-
+      </form>
+    </div>
   </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,9 +1,23 @@
 <?php
 // Create an array of random words
-$words = array("notice", "yarn", "want", "second", "cat", "impolite", "pump", "playground", "blue", "box", "day", "produce", "table", "sheet", "apparatus", "protect", "late", "house", "lumpy", "wooden");
+$words = array("notice", "yarn", "want", "second", "cat", "impolite", "pump", "playground", "blue", "box", "day", "produce", "table", "sheet", "apparatus", "protect", "late", "house", "lumpy", "wooden", "banana", "balloon", "dog", "index", "receipt", "proposal", "dear", "faint", "song", "big", "impact", "crowd", "silk", "poem", "define", "budget", "cow", "chicken", "crime", "stock", "arrival", "high", "portrait", "police", "afford");
 
 // Create an array of random symbols
 $symbols = array("!", "@", "#", "$", "%", "?", "&", "*");
+
+/*
+$err_message = "";
+//error message
+if (isset($_POST["number_of_words"])) {
+  if (!is_numeric($_POST["number_of_words"])) {
+    $err_message = "Please enter a valid number";
+    $password = "";
+    return false;
+  } else {
+    $err_message = "";
+  }
+}
+*/
 
 // Default word total
 $word_total = 4;
@@ -38,21 +52,27 @@ for ($i = 0; $i < $word_total; $i++) {
 Add number at the end of a random word if checked
 */
 if (isset($_POST["add_number"])) {
-  $add_number = rand(1, 9);
-  $num_place = rand(0, count($password_words)-1);
-  $password_words[$num_place] = $password_words[$num_place] . $add_number;
+  $num_qty = $_POST["additional_numbers"];
+  for ($i = 1; $i <= $num_qty; $i++) {
+    $add_number = rand(1, 9);
+    $num_place = rand(0, count($password_words)-1);
+    $password_words[$num_place] = $password_words[$num_place] . $add_number;
+  }
 }
 
 /*
 Add symbol at the end of a random word if checked
 */
 if (isset($_POST["add_symbol"])) {
-  $add_symbol = $symbols[rand(0, count($symbols)-1)];
-  $sym_place = rand(0, count($password_words)-1);
-  $password_words[$sym_place] = $password_words[$sym_place] . $add_symbol;
+  $sym_qty = $_POST["additional_symbols"];
+  for ($i = 1; $i <= $sym_qty; $i++) {
+    $add_symbol = $symbols[rand(0, count($symbols)-1)];
+    $sym_place = rand(0, count($password_words)-1);
+    $password_words[$sym_place] = $password_words[$sym_place] . $add_symbol;
+  }
 }
 
 /*
 Join array elements with "-" string to get final password
 */
-$password = implode ("-", $password_words);
+$password = implode("-", $password_words);
